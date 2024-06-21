0be4b6a435444f9ab54310207a0b6144e01d9b4a
Trent <trent.buyert@outlook.com>
2024-06-20 21:06:06 -0500

    Isolated column names and rows of stats. Need to pair them with team names and rankings.

--- espn_scraper.py
+++ espn_scraper.py
@@ -27,16 +27,36 @@ for _ in range(7):
 time.sleep(2)
 html_content = driver.page_source
 soup = BeautifulSoup(html_content, "html.parser")
-table = soup.find("tbody")
-rows = table.find_all("tr")
-ppg_ranking = []
+columns = []
+thead = soup.find_all("thead")[1]
+tr = thead.find("tr")
+ths = tr.find_all("th")
+first_title = ths[0].find("div").text.strip()
+columns.append(first_title)
+for th in ths[1:]:
+    title = th.find("a").text.strip()
+    columns.append(title)
+stats_table = soup.find_all("tbody")[1]
+rows_stats = stats_table.find_all('tr', class_='Table__TR')
+stats_grid = []
+for row in rows_stats:
+    stat_cells = row.find_all('td', class_='Table__TD')
+    stats_row = [float(cell.get_text(strip=True)) for cell in stat_cells]
+    stats_grid.append(stats_row)
+for y in range(len(stats_grid)):
+    for x in range(len(columns)):
+        print(f"{columns[x]} {stats_grid[y][x]}")
 ppg_stats = []
-for row in rows[:-1]:
-    team_ranking = row.find('td', class_='Table__TD').text.strip()
-    team_name = row.find_all('a', class_='AnchorLink')[1].text.strip()
-    ppg_ranking.append({'ppg_rank': team_ranking, 'ppg_team name': team_name})
-
-df_ppg_rankings = pd.DataFrame(ppg_ranking)
-print(df_ppg_rankings)
+# table = soup.find("tbody")
+# rows = table.find_all("tr")
+# ppg_ranking = []
+# ppg_stats = []
+# for row in rows[:-1]:
+#     team_ranking = row.find('td', class_='Table__TD').text.strip()
+#     team_name = row.find_all('a', class_='AnchorLink')[1].text.strip()
+#     ppg_ranking.append({'ppg_rank': team_ranking, 'ppg_team name': team_name})
+#
+# df_ppg_rankings = pd.DataFrame(ppg_ranking)
+# print(df_ppg_rankings)
 
 driver.close()
