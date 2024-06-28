33fddcd6e87ecdab4c9af924cc17eabcebca9f11
Trent <trent.buyert@outlook.com>
2024-06-27 23:53:12 -0500

    Added team differential stat functionality and csv exports.

--- espn_scraper.py
+++ espn_scraper.py
@@ -91,14 +91,20 @@ def create_team_stats_dataframe(team_rankings, column_names, rows_of_stats):
 
 # URLs and additional pages list
 ncaa_team_stats_url = "https://www.espn.com/mens-college-basketball/stats/team"
+differential_team_stats_url = "https://www.espn.com/mens-college-basketball/stats/team/_/view/differential"
 defensive_team_stats_url = "https://www.espn.com/mens-college-basketball/stats/team/_/view/opponent/season/"
 additional_pages_url = "https://www.espn.com/mens-college-basketball/stats/team/_/season/"
-additional_pages = ['2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014',
+additional_pages = ['2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014',
                     '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004']
 
+# url = differential_team_stats_url0
+# html_content = fetch_additional_pages_stats(url)
+# columns, stats_grid, ppg_ranking = parse_stats(html_content)
+# df = create_team_stats_dataframe(ppg_ranking, columns, stats_grid)
+# df.to_csv(f"2024_season_team_differential_stats.csv")
 for year in additional_pages:
-    url = defensive_team_stats_url + year
+    url = f"{differential_team_stats_url}/season/{year}"
     html_content = fetch_additional_pages_stats(url)
     columns, stats_grid, ppg_ranking = parse_stats(html_content)
     df = create_team_stats_dataframe(ppg_ranking, columns, stats_grid)
-    df.to_csv(f"{year}_regular_season_team_defensive_stats.csv")
+    df.to_csv(f"{year}_season_team_differential_stats.csv")
